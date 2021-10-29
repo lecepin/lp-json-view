a71845fe5f4e45755e348606b8109ea4392fe601
lecepin <383810086@qq.com>
2021-10-29 17:42:03 +0800

    fix: mout el

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,7 +1,15 @@
 "use strict";
 
 (function () {
-  const div = document.createElement("div");
-  div.id = "___lp-json-view-root";
-  document.body.appendChild(div);
+  if (!document.getElementById("___lp-json-view-root")) {
+    let div = document.createElement("div");
+    div.id = "___lp-json-view-root";
+    document.body.appendChild(div);
+  }
+
+  if (!document.getElementById("___lp-json-view-root2")) {
+    let div = document.createElement("div");
+    div.id = "___lp-json-view-root2";
+    document.body.appendChild(div);
+  }
 })();
